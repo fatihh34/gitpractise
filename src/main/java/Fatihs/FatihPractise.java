f838889421e51a3ed84ec1512f522c53e156167a
Fatih I <117350581+fatihh34@users.noreply.github.com>
2023-02-22 22:57:36 +0300

    v1

---
+++ src/main/java/Fatihs/FatihPractise.java
@@ -0,0 +1,11 @@
+package Fatihs;
+
+public class FatihPractise {
+
+    public void deneme(){
+        System.out.println("hello world");
+    }
+    public void deneme2(){
+        System.out.println("goodbye world");
+    }
+}
---
+++ src/main/java/Omers/OmerGitPractise.java
@@ -0,0 +1,22 @@
+package Omers;
+
+public class OmerGitPractise {
+    public static void main(String[] args) {
+
+        String omer="nasılsın";
+        int i= 5;
+
+        if (i<4){
+            System.out.println("kucuk");
+        }else
+            System.out.println("buyuk");
+
+        System.out.println("---------");
+
+        System.out.println("hello world");
+        System.out.println("selam");
+    }
+
+}
+
+
